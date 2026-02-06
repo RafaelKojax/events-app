990a83e59c4c22fe4b55e00eacd023157c172a3e
unknown <arieljuniorc@outlook.com>
2026-02-05 21:55:28 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,87 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>Olá</p>
+      <Welcome />
+      <Button
+        title="Clique aqui"
+        onClick={() => {
+          alert("Botão clicado!!!");
+          console.log("Mensagem no console");
+        }}
+        version={1}
+        // size={2}
+        // obj={{
+        //   chave: "valor",
+        // }}
+        // lista={[1, 2, 3]}
+      />
+
+      <Button
+        // title="Clique aqui"
+        isDisabled
+        onClick={() => {}}
+      />
+
+      <Card title="titulo do card">
+        <div>
+          Esse é o corpo do meu card
+        </div>
+      </Card>
+    </>
+  );
+}
+
+export default App;
+
+const Welcome: React.FC = () => {
+  const mensagem = "Olá mundo";
+  const horaAtual = new Date().toLocaleTimeString();
+  return (
+    <div>
+      <h1>
+        {mensagem}!<br /> Agora são: {horaAtual}
+      </h1>
+      <p>{10 + 5}</p>
+    </div>
+  );
+};
+
+interface ButtonProps {
+  title?: string
+  isDisabled?: boolean
+  onClick: () => void
+  version?: 1 | 2 | "3"
+}
+
+function Button({
+    onClick,
+    title = "Titulo padrão",
+    isDisabled = false,
+}: ButtonProps) {
+  return <button 
+    disabled={isDisabled}
+    onClick={onClick}
+    >
+      {title}
+  </button>;
+}
+
+interface CardProps {
+  title: string,
+  children: React.ReactNode
+}
+
+const Card: React.FC<CardProps> = ({
+  children,
+  title
+})=>{
+  return <div>
+    <h2>{title}</h2>
+    <div>
+      {children}
+    </div>
+  </div>
+}
